1f1fe8fda2b0fbb73cec3a2adf676ddd589d1864
Vortegag <ortegagomez4@gmail.com>
2021-12-02 15:50:40 +0100

    prueba algunos char

--- GestionEnviroment/src/dominio/HelloWorld.java
+++ GestionEnviroment/src/dominio/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Lo he conseguido putas");
-		System.out.println("¿es esto ciero?");
+		System.out.println("¿es esto cierto?@#|!ç*^");
 		System.out.println("Hola");
 	}
 }
