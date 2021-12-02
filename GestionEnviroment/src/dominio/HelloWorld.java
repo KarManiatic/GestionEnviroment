db7fe0d9ee2f0560be5ceae00a5b0e1cd32dcd95
KarManiatic <carromnav@outlook.es>
2021-12-02 15:49:05 +0100

    cambio de a rara

--- GestionEnviroment/src/dominio/HelloWorld.java
+++ GestionEnviroment/src/dominio/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Lo he conseguido putas");
-		System.out.println("Â¿es esto ciero?");
+		System.out.println("¿es esto ciero?");
 		System.out.println("Hola");
 	}
 }
