24c2d63188c12b0e51c3f8a3c2279ee9213bac33
KarManiatic <carromnav@outlook.es>
2021-12-11 17:18:20 +0100

    Eliminación de duplicado

--- GestionEnviroment/src/dominio/Cliente.java
+++ GestionEnviroment/src/dominio/Cliente.java
@@ -11,6 +11,8 @@ public class Cliente extends Usuario {
 		this.saldo = 0;
 	}
 
+	//No estoy seguro si esto es así
+	
 	public static Cliente read(String login, String password) throws Exception {
 		return null;
 	}
@@ -18,7 +20,6 @@ public class Cliente extends Usuario {
 	public static int insert() throws Exception {
 		return 0;
 	}
-
 
 	public static int delete() throws Exception {
 		return 0;
